a9269ed279367cea3de0583d32a812a8a4a2a0be
nickdinhh1998 <60845004+nickdinhh1998@users.noreply.github.com>
2020-02-13 22:10:47 +0700

    Add files via upload

---
+++ Password generator.py
@@ -0,0 +1,17 @@
+import string
+import random
+
+
+user_choice = None
+while user_choice == None:
+    try:
+       user_choice = int(input('How many characters you want to have in your password:'))
+       if user_choice < 6:
+           print('Password must be with 6 characters at least')
+           user_choice = None
+    except ValueError:
+        user_choice = None
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters))
+print('Your password is {}'.format(password))
